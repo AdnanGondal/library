46a4b1e6ea41d1edb4c82bb9063d9b1fdaa8ad24
AdnanGondal <adnangondal1@hotmail.com>
2020-08-06 23:41:03 +0100

    table drawn: need to improve its appearance

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 
-let theLibrary = [];
-addBookToLibrary();
+testBook = new Book("Harry Potter","JK Rowling",400,false);
+testBook2 = new Book("Game of Thrones","RR Martin",800,true);
+testBook3 = new Book("Diary of A Wimpy Kid","Author",125,true);
+let theLibrary = [testBook, testBook2,testBook3];
 render()
 
 function Book(title,author,noPages,isRead){
@@ -24,24 +26,70 @@ function addBookToLibrary() {
     theLibrary.push(userBook);
 }
 
+function createTable(book){
+    bookDiv = document.querySelector("#books-div");
+
+    bookTable = document.createElement('table');
+    bookTable.classList.add("table","table-bordered");
+    bookTableBody = document.createElement('tbody');
+    bookDiv.appendChild(bookTable);
+    bookTable.appendChild(bookTableBody);
+    
+    //For the Title Row:
+    rowTitle = document.createElement('tr');
+    nameTitle = document.createElement("th");
+    nameTitle.setAttribute('scope','row')
+    nameTitle.textContent = "Title: ";
+    //nameTitle.classList.add("col-sm-3");
+    textTitle = document.createElement('td')
+    textTitle.classList.add("text-center");
+    textTitle.textContent = book.title;
+    bookTableBody.appendChild(rowTitle);
+    rowTitle.appendChild(nameTitle);
+    rowTitle.appendChild(textTitle);
+
+    //For the Author Row:
+    authorRow = document.createElement('tr');
+    authorName = document.createElement('th');
+    authorName.setAttribute('scope','row');
+    authorName.textContent = "Author: ";
+    authorText = document.createElement('td');
+    authorText.classList.add("text-center");
+    authorText.textContent = book.author;
+    bookTableBody.appendChild(authorRow);
+    authorRow.appendChild(authorName);
+    authorRow.appendChild(authorText);
+
+    //For noOfPages Row:
+    noPagesRow = document.createElement('tr');
+    noPagesName = document.createElement('th');
+    noPagesName.setAttribute('scope','row');
+    noPagesName.textContent = "Number of pages: ";
+    noPagesText = document.createElement('td');
+    noPagesText.classList.add("text-center")
+    noPagesText.textContent = book.noPages;
+    bookTableBody.appendChild(noPagesRow);
+    noPagesRow.appendChild(noPagesName);
+    noPagesRow.appendChild(noPagesText);
+    
+    finalRow = document.createElement('tr');
+    readBut = document.createElement('button');
+    if (book.isRead) {
+        readBut.textContent = "READ";
+        readBut.classList.add("btn", "btn-success")
+    } else {
+        readBut.textContent = "NOT READ";
+        readBut.classList.add("btn", "btn-danger")
+    }
+    bookTableBody.appendChild(finalRow);
+    finalRow.appendChild(readBut);
+
+}
+
 function render(){
    
     theLibrary.forEach(book=>{
-        bookDiv = document.querySelector("#books-div");
-        bookTable = document.createElement('table');
-        bookTable.classList.add("table");
-        bookTableBody = document.createElement('tbody');
-        
-        nameRow = document.createElement('tr');
-        nameTitle = document.createElement("th");
-        nameTitle.setAttribute('scope','row')
-        nameTitle.textContent = "Name: ";
-
-        bookDiv.appendChild(bookTable);
-        bookTable.appendChild(bookTableBody);
-        
-        bookTableBody.appendChild(nameRow);
-        nameRow.appendChild(nameTitle);
+        createTable(book);
     });
+        
 }
-
